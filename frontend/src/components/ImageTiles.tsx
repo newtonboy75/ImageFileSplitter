31d1433c56838090b2ed64033156fc59eadcc461
Newton Tugadi <newtonboy@gmail.com>
2024-07-15 15:15:58 +0800

    fixed grid numbering style

--- frontend/src/components/ImageTiles.tsx
+++ frontend/src/components/ImageTiles.tsx
@@ -1,11 +1,18 @@
 import { ItemVariants } from "../types/types";
 
-const ImageTiles = (props: { images: any; rows: number; cols: number, toggled: boolean }) => {
+const ImageTiles = (props: {
+  images: any;
+  rows: number;
+  cols: number;
+  toggled: boolean;
+}) => {
   const { images, rows, cols, toggled } = props;
-  
+
   const Tiles = () => {
     let imageElement = [];
     let size = rows * cols;
+
+    
 
     const columnVariants: ItemVariants = {
       1: "grid-cols-1",
@@ -16,23 +23,30 @@ const ImageTiles = (props: { images: any; rows: number; cols: number, toggled: b
       6: "grid-cols-6",
       7: "grid-cols-7",
       8: "grid-cols-8",
-      9: "grid-cols-19",
+      9: "grid-cols-9",
       10: "grid-cols-10",
     };
 
+    
     const toggleValue: ItemVariants = {
-        0: "border-0",
-        1: "border-2"
-    }
+      0: "border-0",
+      1: "border-2",
+    };
 
-    let togglerVal = toggled ? 0 : 1
+    let togglerVal = toggled ? 0 : 1;
 
     for (let i = 0; i < size; i++) {
-        const timestamp = Math.random();
-        let imageUri =
-          "http://localhost:5000" + images[i] + "?timestamo=" + timestamp;
-        imageElement.push(<img className={`${toggleValue[togglerVal]}`} key={Math.random()} src={imageUri} />);
-      }
+      const timestamp = Math.random();
+      let imageUri =
+        "http://localhost:5000" + images[i] + "?timestamo=" + timestamp;
+      imageElement.push(
+        <img
+          className={`${toggleValue[togglerVal]}`}
+          key={Math.random()}
+          src={imageUri}
+        />
+      );
+    }
 
     let div = (
       <div className={`bg-gray-300 grid ${columnVariants[cols]}`}>
